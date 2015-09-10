78b7e003ccb59d04e108ca4bb2e719526502f7a7
Karol Moroz <dmuhafc@gmail.com>
2015-09-10 14:43:27 +0200

    Colors, content, locales

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
 class PagesController < ApplicationController
   def home
-    @title = "Strona główna"
+    @title = t("home")
+  end
+
+  def contact
+    @title = t("contact")
+  end
+
+  def offers
+    @title = t("offers")
+  end
+
+  def employer
+    @title = t("employer")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,54 +4,14 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
+  get '/kontakt' => 'pages#contact'
+  get '/angebote' => 'pages#offers', locale: 'de'
+  get '/oferty' => 'pages#offers', locale: 'pl'
+  get '/pracodawca' => 'pages#employer', locale: 'pl'
+  get '/arbeitgeber' => 'pages#employer', locale: 'de'
 
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get "/:locale" => 'pages#home', locale: /pl|de/
+  get "/kontakt/:locale" => "pages#contact", locale: /pl|de/
+  get "/offers/:locale" => "pages#offers", locale: /pl|de/
+  get "/employer/:locale" => "pages#employer", locale: /pl|de/
 end
